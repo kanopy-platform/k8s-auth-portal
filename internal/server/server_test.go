4b9422391480842f46e65462c4f6122378518b49
Colin Hoglund <colinhoglund@gmail.com>
2021-08-24 16:05:39 -0400

    update var

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -9,15 +9,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var s http.Handler
+var testHandler http.Handler
 
 func TestMain(m *testing.M) {
-	s = New()
+	testHandler = New()
 	os.Exit(m.Run())
 }
 
 func TestHandleRoot(t *testing.T) {
 	w := httptest.NewRecorder()
-	s.ServeHTTP(w, httptest.NewRequest("GET", "/", nil))
+	testHandler.ServeHTTP(w, httptest.NewRequest("GET", "/", nil))
 	assert.Equal(t, http.StatusOK, w.Code)
 }
