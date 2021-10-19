471823185c6fac61026305a7758942fa494c2e90
Yuzhou Liu <89029096+yuzhouliu9@users.noreply.github.com>
2021-10-19 12:27:04 -0400

    DEVOPS-2798 Implement CSRF handling (#8)
    add csrf handling between login and callback

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -119,60 +119,130 @@ func TestHandleLoginGet(t *testing.T) {
 func TestHandleLoginPost(t *testing.T) {
 	t.Parallel()
 
-	w := httptest.NewRecorder()
-	server.ServeHTTP(w, httptest.NewRequest("POST", "/login", nil))
-	assert.Equal(t, http.StatusSeeOther, w.Code)
-	assert.NotEmpty(t, w.Result().Cookies()[0])
+	tests := []struct {
+		body           string
+		wantHttpStatus int
+	}{
+		{
+			// empty POST form
+			body:           "",
+			wantHttpStatus: http.StatusBadRequest,
+		},
+		{
+			// empty state
+			body:           "state=",
+			wantHttpStatus: http.StatusBadRequest,
+		},
+		{
+			// valid state
+			body:           "state=test-123",
+			wantHttpStatus: http.StatusSeeOther,
+		},
+	}
+
+	for _, test := range tests {
+		rr := httptest.NewRecorder()
+		req := httptest.NewRequest("POST", "/login", strings.NewReader(test.body))
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+		server.ServeHTTP(rr, req)
+		assert.Equal(t, test.wantHttpStatus, rr.Code)
+
+		if test.wantHttpStatus == http.StatusSeeOther {
+			assert.NotEmpty(t, rr.Result().Cookies()[0])
+		}
+	}
 }
 
 func TestHandleCallbackGet(t *testing.T) {
 	t.Parallel()
 
-	const validCallbackUrl = "/callback?code=testcode"
+	w := httptest.NewRecorder()
+	server.ServeHTTP(w, httptest.NewRequest("GET", "/callback", nil))
+	assert.Equal(t, http.StatusMethodNotAllowed, w.Code)
+}
+
+func TestHandleCallbackPost(t *testing.T) {
+	t.Parallel()
+
+	const (
+		validCallbackUrl = "/callback"
+		sessionState     = "test-state-123"
+		validAuthCode    = "test-code-123"
+	)
 	testNonce = "test-nonce-123"
 
 	tests := []struct {
 		url            string
+		body           string
 		nonce          string
 		wantHttpStatus int
 	}{
 		{
 			// success case
 			url:            validCallbackUrl,
+			body:           fmt.Sprintf("state=%s&code=%s", sessionState, validAuthCode),
 			nonce:          testNonce,
 			wantHttpStatus: http.StatusOK,
 		},
 		{
 			// error in request URL
 			url:            "/callback?code=testcode&error=some-error",
+			body:           fmt.Sprintf("state=%s&code=%s", sessionState, validAuthCode),
+			nonce:          testNonce,
+			wantHttpStatus: http.StatusBadRequest,
+		},
+		{
+			// no state
+			url:            validCallbackUrl,
+			body:           fmt.Sprintf("code=%s", validAuthCode),
+			nonce:          testNonce,
+			wantHttpStatus: http.StatusBadRequest,
+		},
+		{
+			// empty state
+			url:            validCallbackUrl,
+			body:           fmt.Sprintf("state=&code=%s", validAuthCode),
+			nonce:          testNonce,
+			wantHttpStatus: http.StatusBadRequest,
+		},
+		{
+			// POST and session state does not match
+			url:            validCallbackUrl,
+			body:           fmt.Sprintf("state=%s&code=%s", "mismatched-state", validAuthCode),
 			nonce:          testNonce,
 			wantHttpStatus: http.StatusBadRequest,
 		},
 		{
 			// no authorization code
-			url:            "/callback",
+			url:            validCallbackUrl,
+			body:           fmt.Sprintf("state=%s", sessionState),
 			nonce:          testNonce,
 			wantHttpStatus: http.StatusUnauthorized,
 		},
 		{
 			// empty authorization code
-			url:            "/callback?code=",
+			url:            validCallbackUrl,
+			body:           fmt.Sprintf("state=%s&code=", sessionState),
 			nonce:          testNonce,
 			wantHttpStatus: http.StatusUnauthorized,
 		},
 		{
 			// nonce doesn't match expected
 			url:            validCallbackUrl,
-			nonce:          "invalid nonce",
+			body:           fmt.Sprintf("state=%s&code=%s", sessionState, validAuthCode),
+			nonce:          "mismatched-nonce",
 			wantHttpStatus: http.StatusUnauthorized,
 		},
 	}
 
 	for _, test := range tests {
 		rr := httptest.NewRecorder()
-		req := httptest.NewRequest("GET", test.url, nil)
+		req := httptest.NewRequest("POST", test.url, strings.NewReader(test.body))
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
 		session := server.getSession(req)
+		session.Values["state"] = sessionState
 		session.Values["nonce"] = test.nonce
 
 		server.ServeHTTP(rr, req)
