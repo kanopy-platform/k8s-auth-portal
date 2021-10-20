f6130c34cad9787f33f2b3a50b58499c0d2ed176
Yuzhou Liu <89029096+yuzhouliu9@users.noreply.github.com>
2021-10-20 14:03:02 -0400

    working 'make docker-run' and revise input option --session-secret-filepath (#11)

--- internal/server/options.go
+++ internal/server/options.go
@@ -16,10 +16,14 @@ func WithSessionName(name string) ServerFuncOpt {
 	}
 }
 
-func WithSessionSecret(secret string) ServerFuncOpt {
+func WithSessionSecret(filePath string) ServerFuncOpt {
 	return func(s *Server) error {
-		if secret != "" {
-			s.sessionSecret = secret
+		if filePath != "" {
+			data, err := os.ReadFile(filePath)
+			if err != nil {
+				return err
+			}
+			s.sessionSecret = string(data)
 		}
 
 		return nil
