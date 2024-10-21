27735dbbc7353b44a99c97914ae66686b300fc37
danpetitclerc <danny.petitclerc@gmail.com>
2024-10-21 15:47:21 -0400

    Add PKCE flow auth  (#23)
    * update login and redirect with PKCE flow
    
    * update err status code
    
    * go mod tidy
    
    * go mod
    
    * update dependencies
    
    * missing go sum entry
    
    * tidy
    
    * update dockerfile, drone pipeline
    
    * linter stuff
    
    * adjust test and mocks for new PKCE opt
    
    * update license step to use latest image
    
    * missing licenses
    
    * additionnal "other" licenses
    
    * licenses fmt
    
    * update license-check image to tagged version
    
    * update golangci-lint to tagged version
    
    * Remove dependency changes from this PR
    
    * tagging from branch, will revert
    
    * no autotag
    
    * explicitly add code_challenge_method param
    
    * simplify test tagging
    
    * remove randomstr, pass state directly
    
    * update dockerfile, disable CGO
    
    * trying with new tag
    
    * cleanup server code
    
    * revert drone changes
    
    * fix unsafe html template error rendering all state values as ZgotmplZ
    
    ---------
    
    Co-authored-by: Danny Petitclerc <danny.petitclerc@mongodb.com>
    Co-authored-by: wwalter <53450727+whwalter@users.noreply.github.com>

--- internal/server/server.go
+++ internal/server/server.go
@@ -69,10 +69,16 @@ func New(opts ...ServerFuncOpt) (*Server, error) {
 		return nil, err
 	}
 
+	funcMap := template.FuncMap{
+		"formValue": func(s string) template.HTMLAttr {
+			return template.HTMLAttr(fmt.Sprintf("value=%q", s))
+		},
+	}
+
 	// set defaults
 	s := &Server{
 		Router:              mux.NewRouter(),
-		template:            template.Must(template.ParseFS(embeddedFS, "templates/*.tmpl")),
+		template:            template.Must(template.New("view_index.tmpl").Funcs(funcMap).ParseFS(embeddedFS, "templates/*.tmpl")),
 		sessionName:         "k8s-auth-portal-session",
 		sessionSecret:       randSecret,
 		kubectlClientID:     "kubectl",
@@ -318,11 +324,6 @@ func (s *Server) handleLogin() http.HandlerFunc {
 		// Pass it into /login, which saves it in the session cookie.
 		// The /callback handler will verify the passed in state matches that from the session.
 		// This achieves the equivalent functionality as using "state" in URL.
-		randomStr, err := random.SecureString(32)
-		if err != nil {
-			logAndError(w, http.StatusInternalServerError, err, "error generating random string")
-			return
-		}
 
 		state := r.PostFormValue("state")
 		if state == "" {
@@ -339,12 +340,18 @@ func (s *Server) handleLogin() http.HandlerFunc {
 		}
 		session.Values["nonce"] = nonce
 
+		// generate a PKCE code verifier
+		codeVerifier := oauth2.GenerateVerifier()
+
+		// store code_verifier for token exchange
+		session.Values["code_verifier"] = codeVerifier
+
 		if err := session.Save(r, w); err != nil {
 			logAndError(w, http.StatusInternalServerError, err, "error saving session")
 			return
 		}
 
-		http.Redirect(w, r, s.oauth2Config.AuthCodeURL(randomStr, oidc.Nonce(nonce)), http.StatusSeeOther)
+		http.Redirect(w, r, s.oauth2Config.AuthCodeURL(state, oidc.Nonce(nonce), oauth2.S256ChallengeOption(codeVerifier)), http.StatusSeeOther)
 	}
 }
 
@@ -376,8 +383,14 @@ func (s *Server) handleCallback() http.HandlerFunc {
 			return
 		}
 
-		// convert authorization code into an OAuth2 token
-		oauth2Token, err := s.oauth2Config.Exchange(oidcContext, code)
+		codeVerifier, ok := session.Values["code_verifier"].(string)
+		if !ok {
+			logAndError(w, http.StatusUnauthorized, fmt.Errorf("code verifier not found in session"), "error retrieving code verifier")
+			return
+		}
+
+		// convert authorization code into an OAuth2 token with added PKCE verifier opt
+		oauth2Token, err := s.oauth2Config.Exchange(oidcContext, code, oauth2.VerifierOption(codeVerifier))
 		if err != nil {
 			logAndError(w, http.StatusUnauthorized, err, "error converting code to token")
 			return
